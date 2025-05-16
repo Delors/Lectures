f0df52dd8c254c031bc4e525c68d39b68acb961c
Michael Eichberg <mail@michael-eichberg.de>
2025-05-16 12:41:54 +0200

    fixed typos

--- prog-java-basics/code/IfElseReformatted.java
+++ prog-java-basics/code/IfElseReformatted.java
@@ -1,17 +1,12 @@
 void main() {
     println("Anzahl der Tage in einem Monat");
 
-    var month = Integer.parseInt(readln("Welchen Monate haben wir(1-12)? "));
+    var month = Integer.parseInt(readln("Welchen Monat haben wir(1-12)? "));
 
     int days = 31;
-    if (month == 2 && readln("Schaltjahr (j/n)? ").charAt(0) == 'j') 
-        days = 29;
-    else // "nur" umformatiert
-        if (month == 2)
-            days = 28;
-        else 
-            if (month == 4)
-                days = 30;
-            // else ...
+    if (month == 2 && readln("Schaltjahr (j/n)? ").charAt(0) == 'j') days = 29;
+    else if (month == 2) days = 28; // "nur" umformatiert
+    else if (month == 4) days = 30;
+    // else ...
     println("days=" + days);
 }
--- prog-java-basics/code/Switch.java
+++ prog-java-basics/code/Switch.java
@@ -1,15 +1,15 @@
 void main() {
-    var month = Integer.parseInt(readln("Welchen Monate haben wir(1-12)? "));
+    var month = Integer.parseInt(readln("Welchen Monat haben wir(1-12)? "));
     int days = 31;
     switch (month) { // vor Java 14!
         case 2:
-            if (readln("Schaltjahr (j/n)? ").charAt(0) == 'j')
-                days = 29;
-            else
-                days = 28;
+            if (readln("Schaltjahr (j/n)? ").charAt(0) == 'j') days = 29;
+            else days = 28;
             break;
         case 4:
-        case 6: case 9: case 11: // <= possible, but unusual formatting
+        case 6:
+        case 9:
+        case 11: // <= possible, but unusual formatting
             days = 30;
             break;
     }
